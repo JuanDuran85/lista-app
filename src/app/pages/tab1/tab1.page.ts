84959a5752fa2f536ecdaec8371bb41ac38615d0
JuanDuran85 <duran85juan@gmail.com>
2021-05-19 22:50:20 -0400

    nuevas rutas y paginas a mostrar

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeseosService } from './../../services/deseos.service';
 import { ListaInter } from './../../interfaces/lista-item.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -11,10 +12,15 @@ export class Tab1Page implements OnInit {
 
   public listaDeseos: ListaInter[] = [];
 
-  constructor(public deseosSer: DeseosService) {}
+  constructor(public deseosSer: DeseosService, private router: Router) {}
 
   ngOnInit(): void {
     this.listaDeseos = this.deseosSer.listas;
+  }
+
+  agregarLista(){
+    console.log('presion...');
+    this.router.navigateByUrl('/tabs/tab1/agregar');
   }
 
 }
